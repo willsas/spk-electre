c80ef9211de20dcb51eb9ef97074c340131449e0
willa <willa.saskara@vidio.com>
2024-05-05 21:41:51 +0700

    Initial Commit

---
+++ spk-electre/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  spk-electre
+//
+//  Created by VIDIO on 05/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
